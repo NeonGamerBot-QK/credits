bf85078b42a7c59f69d4d120acff3fb3492ecad1
Saahil <neon@saahild.com>
2024-07-03 23:18:33 -0400

    Init

---
+++ index.js
@@ -0,0 +1,7 @@
+// once loaded log
+console.log(new Array(40).join('='))
+console.log(`CC Neon @ ${new Date().getFullYear()}`)
+console.log(`This code is private and was made by Neon.`)
+console.log(`Do not resell this code or use it if someone re sold it.`)
+console.log(`For inquries about re-sold code contact neon@saahild.com`)
+console.log(new Array(40).join('='))
